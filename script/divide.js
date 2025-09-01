876409edf2fd3dda04d2d6a5a17184d35fbf311b
Bj456 <165272492+Bj456@users.noreply.github.com>
2025-09-01 09:39:28 +0530

    Update divide.js

--- script/divide.js
+++ script/divide.js
@@ -1,54 +1,47 @@
 const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const option3 = document.getElementById("option3");
-const audio = document.getElementById("wrong");
+const wrongAudio = document.getElementById("wrong");
+const correctAudio = document.getElementById("correct");
+
 let answer = 0;
 
-function generateEquation(){
-    let num2 = Math.floor(Math.random() * 12) + 1;
-    let num1 = num2 * (Math.floor(Math.random() * 12) + 1);
-    let dummyAnswer1 = Math.floor(Math.random() * 12) + 1;
-    let dummyAnswer2 = Math.floor(Math.random() * 12) + 1;
+function generateEquation() {
+    let num2 = Math.floor(Math.random() * 12) + 1; // divisor never 0
+    let answerReal = Math.floor(Math.random() * 12);
+    let num1 = num2 * answerReal;
 
-    answer = num1 / num2;
+    let dummy1 = Math.floor(Math.random() * 12);
+    let dummy2 = Math.floor(Math.random() * 12);
+
+    answer = answerReal;
 
     document.getElementById("num1").innerHTML = num1;
     document.getElementById("num2").innerHTML = num2;
 
-    let allAnswers = [answer, dummyAnswer1, dummyAnswer2];
-    let switchAnswers = [];
+    let allAnswers = [answer, dummy1, dummy2];
+    let shuffled = [];
 
-    for(i = allAnswers.length; i--;){
-        switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i+1)), 1)[0]);
+    for (let i = allAnswers.length; i > 0; i--) {
+        shuffled.push(allAnswers.splice(Math.floor(Math.random() * i), 1)[0]);
     }
 
-    option1.innerHTML = switchAnswers[0];
-    option2.innerHTML = switchAnswers[1];
-    option3.innerHTML = switchAnswers[2];
+    option1.innerHTML = shuffled[0];
+    option2.innerHTML = shuffled[1];
+    option3.innerHTML = shuffled[2];
 }
 
-option1.addEventListener("click", function(){
-    if(parseInt(option1.innerHTML) === answer){
+function checkAnswer(option) {
+    if (parseInt(option.innerHTML) === answer) {
+        correctAudio.play();
         generateEquation();
     } else {
-        audio.play();
+        wrongAudio.play();
     }
-});
+}
 
-option2.addEventListener("click", function(){
-    if(parseInt(option2.innerHTML) === answer){
-        generateEquation();
-    } else {
-        audio.play();
-    }
-});
-
-option3.addEventListener("click", function(){
-    if(parseInt(option3.innerHTML) === answer){
-        generateEquation();
-    } else {
-        audio.play();
-    }
-});
+option1.addEventListener("click", () => checkAnswer(option1));
+option2.addEventListener("click", () => checkAnswer(option2));
+option3.addEventListener("click", () => checkAnswer(option3));
 
 generateEquation();
